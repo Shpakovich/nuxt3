eed63ddc01c0a25e9a0b7621bd7f7b67be361ca7
dshmakov <dshmakov@voximplant.com>
2022-05-06 19:09:35 +1000

    init nuxt3 project

---
+++ layouts/default.vue
@@ -0,0 +1,23 @@
+<template>
+  <header>
+    <h1>Nuxt 3</h1>
+  </header>
+  <main>
+    <slot />
+  </main>
+  <footer>
+    <p>
+      {{ new Date().getFullYear() }}
+    </p>
+  </footer>
+</template>
+
+<script>
+export default {
+  name: "default"
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <NuxtLayout name='default'>
+    <p>Index Page</p>
+  </NuxtLayout>
+</template>
+
+<script>
+import {defineComponent} from "@vue/runtime-core";
+
+export default defineComponent({
+
+})
+</script>
+
+<style scoped>
+
+</style>
