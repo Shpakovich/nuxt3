549d1901651b8cd1b54d3a945b398e0fc3c4a5ad
dshmakov <dshmakov@voximplant.com>
2022-05-09 21:01:05 +1000

    add vue GoogleMap module

---
+++ components/GoogleMaps.vue
@@ -0,0 +1,28 @@
+<template>
+  <GoogleMap
+      api-key="YOUR_GOOGLE_MAPS_API_KEY"
+      style="width: 100%; height: 500px"
+      :center="center"
+      :zoom="15"
+  >
+    <Marker :options="{ position: center }" />
+  </GoogleMap>
+</template>
+
+<script lang="ts">
+import {defineComponent} from "@vue/runtime-core";
+import { GoogleMap, Marker } from 'vue3-google-map'
+
+export default defineComponent({
+  components: { GoogleMap, Marker },
+  setup() {
+    const center = { lat: 40.689247, lng: -74.044502 }
+
+    return { center }
+  },
+})
+</script>
+
+<style scoped>
+
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,14 +1,18 @@
 <template>
   <NuxtLayout name='default'>
     <p>Index Page</p>
+    <GoogleMaps />
   </NuxtLayout>
 </template>
 
-<script>
+<script lang="ts">
 import {defineComponent} from "@vue/runtime-core";
+import GoogleMaps from "../components/GoogleMaps";
 
 export default defineComponent({
-
+    components: {
+      GoogleMaps
+    }
 })
 </script>
 
