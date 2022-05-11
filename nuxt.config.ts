eea579f3101ca56dcfce55d04dfc2095af22bfdf
dshmakov <dshmakov@voximplant.com>
2022-05-11 14:11:50 +1000

    add default css for mapboxgl

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,8 @@ import { defineNuxtConfig } from 'nuxt'
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-
+    css: [
+        // CSS file for mapboxgl
+        '@/assets/styles/mapboxgl.css',
+    ]
 })
